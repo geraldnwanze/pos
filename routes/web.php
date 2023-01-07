984e92d1a6378282514f10b4bbd9979f7dc373f2
geraldon1997 <geraldon1997@gmail.com>
2023-01-07 16:45:36 +0100

    first commit

---
+++ routes/web.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::view('/', 'auth.login')->name('home');
+
+Route::fallback(function () {
+    auth()->logout();
+    session()->regenerate();
+    return redirect()->route('home');
+});
